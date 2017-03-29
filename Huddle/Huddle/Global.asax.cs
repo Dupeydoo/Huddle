e38f9bb6e75d295ccb4444ee73a0f250f19af72d
Dupeydoo <jrmd202@exeter.ac.uk>
2017-03-29 19:05:57 +0100

    Added required registration for UI validation via jquery. Good stuff!

--- Huddle/Huddle/Global.asax.cs
+++ Huddle/Huddle/Global.asax.cs
@@ -1,21 +1,60 @@
 ﻿using System;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.UI;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
 namespace Huddle
 {
-    public class Global : HttpApplication
+    public class Global : System.Web.HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+
+        protected void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/jquery-3.1.1.min.js"
+                }
+            );
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
         }
     }
 }
