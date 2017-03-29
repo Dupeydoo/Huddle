6b6a47e40667d5d9f2fac21acc5977732e856409
Dupeydoo <jrmd202@exeter.ac.uk>
2017-03-29 08:19:02 +0100

    More css lol and customising main, added FA for some nice icons

--- Huddle/Huddle/MasterPages/Main.Master.designer.cs
+++ Huddle/Huddle/MasterPages/Main.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Huddle.MasterPages {
         protected global::System.Web.UI.ScriptManager MasterScript;
         
         /// <summary>
+        /// lblChooseTheme control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblChooseTheme;
+        
+        /// <summary>
         /// ChooseTheme control.
         /// </summary>
         /// <remarks>
