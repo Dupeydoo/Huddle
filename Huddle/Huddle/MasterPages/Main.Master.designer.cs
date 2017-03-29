3b01bf8f8fce8a5f64776438f48ba9861dac06e9
Dupeydoo <jrmd202@exeter.ac.uk>
2017-03-29 18:40:58 +0100

    Added some images, images look nice, will need compression later. Rebalanced some icon styles.

--- Huddle/Huddle/MasterPages/Main.Master.designer.cs
+++ Huddle/Huddle/MasterPages/Main.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Huddle.MasterPages {
         protected global::System.Web.UI.ScriptManager MasterScript;
         
         /// <summary>
+        /// BannerImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image BannerImage;
+        
+        /// <summary>
         /// lblChooseTheme control.
         /// </summary>
         /// <remarks>
