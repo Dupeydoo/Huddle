9dbaf1a7398880279b1d45e206a7913bfc6042f9
Dupeydoo <jrmd202@exeter.ac.uk>
2017-04-05 18:29:40 +0100

    Ended up fixing the hover menu issue without js, hoorah!

--- Huddle/Huddle/Scripts/Huddle.js
+++ Huddle/Huddle/Scripts/Huddle.js
@@ -1,17 +1,1 @@
-﻿var hoverContent = $(".container-fluid .navbar-inverse .navbar-nav .hover-wrapper .hover-content");
-
-hoverContent.mouseenter(function() {
-    var menuParent = hoverContent.parent();
-
-    if(!menuParent.hasClass("active")) {
-        menuParent.addClass("submenu-parent");
-    }
-})
-
-hoverContent.mouseleave(function() {
-    var menuParent = hoverContent.parent();
-
-    if(menuParent.hasClass("submenu-parent")) {
-       menuParent.removeClass("submenu-parent");
-    }
-})
+﻿
