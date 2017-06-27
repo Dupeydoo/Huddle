e4b7c067a1fa6168756cd6ad3430681465edde00
James <jrmd202@exeter.ac.uk>
2017-06-27 14:01:17 +0100

    Wrote threadsdata and refactored for lightweight ef wrapper free objects.

--- Huddle/Huddle.Data/ModelBinding/ThreadsData.cs
+++ Huddle/Huddle.Data/ModelBinding/ThreadsData.cs
@@ -16,22 +16,51 @@ namespace Huddle.Data.ModelBinding
         {
             using (HuddleEntities entities = new HuddleEntities())
             {
-                return (from threads in entities.Threads
+                return  (from threads in entities.Threads
                         where threads.CategoryId == id
                         orderby threads.DateModified descending
                         select threads);
             }
         }
 
-        public IQueryable<Thread> GetCategoryThreadsFromDB(int id, bool isSticky)
+        public IQueryable<ObjectThread> GetCategoryThreadsFromDB(int id, bool isSticky)
         {
             IQueryable<Thread> threads = GetCategoryThreadsFromDB(id);
+            //Parse threads to custom lightweight ef wrapper free objects
+            IQueryable<ObjectThread> output = ParseThreadSelect(threads);
 
             if (isSticky)
             {
-                return threads.Where(thread => thread.IsSticky == true);
+                return output.Where(thread => thread.IsSticky == true);
             }
-            return threads.Where(thread => thread.IsSticky == false);
+            return output.Where(thread => thread.IsSticky == false);
+        }
+
+        private IQueryable<ObjectThread> ParseThreadSelect(IQueryable<Thread> threads)
+        {
+            List<ObjectThread> output = new List<ObjectThread>();
+
+            for(int t = 0 ; t < threads.Count() ; t++)
+            {
+                Thread thread = threads.ElementAt(t);
+
+                output.Add(
+                    new ObjectThread
+                    {
+                        CategoryId = thread.CategoryId,
+                        CreatedBy = thread.CreatedBy,
+                        DateCreated = thread.DateCreated,
+                        DateModified = thread.DateModified,
+                        Id = thread.Id,
+                        IsSticky = thread.IsSticky,
+                        ModifiedBy = thread.ModifiedBy,
+                        Title = thread.Title,
+                        Views = thread.Views
+                    }
+                );
+            }
+
+            return output.AsQueryable();
         }
     }
 }
