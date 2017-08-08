5ee9e6d0690be404e44002b807775028eb400e30
James <jrmd202@exeter.ac.uk>
2017-08-08 10:08:11 +0100

    Docs for huddlecommon

--- Huddle/Huddle.Objects/Common/HuddleCommon.cs
+++ Huddle/Huddle.Objects/Common/HuddleCommon.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Huddle.Objects.Common
 {
+    /*
+     * A class for use around Huddle with common methods
+     * 
+     * @author  James
+     * @version 1.0.0
+    */
     public static class HuddleCommon
     {
+        /*
+         * This method checks to see if integer x is divisible by integer y
+         * 
+         * @param    x  the first integer
+         * @param    y  the second integer
+         * @returns  true if x is divisible by y
+         * @author   James
+         * @version  1.0.0
+        */
         public static bool IsDivisible(int x, int y)
         {
             return x % y == 0;
